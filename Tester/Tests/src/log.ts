bb47faa0bc4010f79ba11226ad85a35c077c31b7
Fabio Luiz Alvares Osti <fabio.osti@hotmail.com>
2023-01-05 23:39:45 -0300

    Log formatting

--- Tester/Tests/src/log.ts
+++ Tester/Tests/src/log.ts
@@ -7,6 +7,7 @@ interface TestResult {
 }
 
 const tests: TestResult[] = []
+const formatOpt = { minimumIntegerDigits: 2, useGrouping: false }
 
 export function getTestAccumulator(title: string) {
 	return function (body: any, success: boolean) {
@@ -20,9 +21,9 @@ export function getTestAccumulator(title: string) {
 
 function writeMD(filename: string) {
 	const content = tests.reduce(
-		(acc, cur) =>
+		(acc, cur, i) =>
 			`${acc}` +
-			`## [${cur.success ? '✔️' : '❌'}] ${cur.title}:\n` +
+			`## ${(i + 1).toLocaleString('en-US', formatOpt)}: [${cur.success ? '✔️' : '❌'}] ${cur.title}:\n` +
 			`\`\`\`json\n` +
 			`${JSON.stringify(cur.body, null, '\t')}\n` +
 			`\`\`\`\n`,
@@ -34,16 +35,23 @@ function writeMD(filename: string) {
 }
 
 function writeTXT(filename: string) {
-	const line = "-".repeat(38)
+	const line = "+".repeat(37)
+	var divisor: string
 	const content = tests.reduce(
-		(acc, cur) =>
-			`${acc}<${line}[${cur.success ? 'V' : 'X'}]${line}>\n${cur.title}:\n${JSON.stringify(cur.body, null, '\t')}\n`,
+		(acc, cur, i) =>
+			acc +
+			(divisor = `<${line}[${(i + 1).toLocaleString('en-US', formatOpt)}]${line}>\n`) +
+			`${cur.success ? 'PASSED' : 'FAILED'}: ${cur.title}\n` +
+			`${"-".repeat(80).trimEnd()}\n` +
+			`${JSON.stringify(cur.body, null, '\t')}\n` +
+			divisor +
+			"\n\n",
 		""
-	)
+	).trimEnd()
 	fs.writeFile(`./results/${filename}.txt`, content, (err) => {
 		if (err !== null) console.log(err)
 	})
-	
+
 }
 
 function writeJSON(filename: string) {
@@ -52,12 +60,32 @@ function writeJSON(filename: string) {
 	})
 }
 
-export function printAccumulatedTests({ writeToTxt, writeToJson, writeToMd }: { writeToTxt?: boolean; writeToJson?: boolean; writeToMd?: boolean }) {
+function writeConsole() {
+	const columns = (process.stdout.columns % 2) == 0 ? process.stdout.columns : process.stdout.columns - 1
+	const line = "+".repeat((columns - 6) / 2)
+	tests.forEach((cur, i) => {
+		const divisor = `<${line}[${(i + 1).toLocaleString('en-US', formatOpt)}]${line}>`
+		console.log(divisor)
+		console.log(`${cur.success ? 'PASSED' : 'FAILED'}: ${cur.title}`)
+		console.log("-".repeat(columns))
+		console.log(cur.body)
+		console.log(divisor)
+		console.log("\n\n")
+	});
+}
 
-	const filename = (new Date(Date.now())).toJSON().replaceAll('.','').replaceAll(':','').replaceAll('-','')
-	
+export function printAccumulatedTests(
+	{ writeToTxt, writeToJson, writeToMd, writeToConsole }: {
+		writeToTxt?: boolean;
+		writeToJson?: boolean;
+		writeToMd?: boolean;
+		writeToConsole?: boolean;
+	}
+) {
+	const filename = (new Date(Date.now())).toJSON().replaceAll('.', '').replaceAll(':', '').replaceAll('-', '')
+
 	if (writeToTxt) writeTXT(filename)
 	if (writeToJson) writeJSON(filename)
 	if (writeToMd) writeMD(filename)
-
+	if (writeToConsole) writeConsole()
 }
