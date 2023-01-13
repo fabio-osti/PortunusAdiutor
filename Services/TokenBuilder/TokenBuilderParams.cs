4a10acc4ed27e693345a5b0d87581baa2bbbedb5
Fabio Luiz Alvares Osti <fabio.osti@hotmail.com>
2023-01-13 16:46:35 -0300

    TokenBuilderParams constructor using Configuration

--- Services/TokenBuilder/TokenBuilderParams.cs
+++ Services/TokenBuilder/TokenBuilderParams.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace PortunusAdiutor.Services.TokenBuilder;
@@ -13,6 +16,95 @@ namespace PortunusAdiutor.Services.TokenBuilder;
 /// </remarks>
 public class TokenBuilderParams
 {
+	/// <summary>
+	/// 	Initialize an instance of <see cref="TokenBuilderParams"/>.
+	/// </summary>
+	public TokenBuilderParams() { }
+
+	/// <summary>
+	/// 	Initialize an instance of <see cref="TokenBuilderParams"/>
+	/// 	using an <see cref="IConfiguration"/> object as base and
+	///		<see cref="Encoding.GetBytes(string)"/> to extract the bytes.
+	/// </summary>
+	/// 
+	/// <param name="config">
+	/// 	An <see cref="IConfiguration"/> instance that 
+	/// 	have the section "JWT" defined.
+	/// </param>
+	/// 
+	/// <remarks>
+	/// 	<list type="table">
+	/// 		<listheader>
+	/// 			<term>Property</term>
+	/// 			<description>Configuration Key</description>
+	/// 		</listheader>
+	/// 		<item>
+	/// 			<term><see cref="SigningKey"/></term>
+	/// 			<description>"SSK"</description>
+	/// 		</item>
+	/// 		<item>
+	/// 			<term><see cref="EncryptionKey"/></term>
+	/// 			<description>"SEK"</description>
+	/// 		</item>
+	/// 	</list>
+	/// </remarks>
+	/// 
+	/// <exception cref="ArgumentNullException">
+	/// 	Thrown if "SSK" or "SEK" are not defined on the "JWT" section
+	/// 	of <paramref name="config"/>
+	/// </exception>
+	[SetsRequiredMembers]
+	public TokenBuilderParams(IConfiguration config)
+		: this(config, Encoding.Unicode.GetBytes) { }
+
+	/// <summary>
+	/// 	Initialize an instance of <see cref="TokenBuilderParams"/>
+	/// 	using an <see cref="IConfiguration"/> object as base and
+	///		<paramref name="converter"/> to extract the bytes.
+	/// </summary>
+	/// 
+	/// <param name="config">
+	/// 	An <see cref="IConfiguration"/> instance that 
+	/// 	have the section "JWT" defined.
+	/// </param>
+	/// 
+	/// <param name="converter">
+	/// 	Function to extract the bytes from the security key string.
+	/// </param>
+	/// 
+	/// <remarks>
+	/// 	<list type="table">
+	/// 		<listheader>
+	/// 			<term>Property</term>
+	/// 			<description>Configuration Key</description>
+	/// 		</listheader>
+	/// 		<item>
+	/// 			<term><see cref="SigningKey"/></term>
+	/// 			<description>"SSK"</description>
+	/// 		</item>
+	/// 		<item>
+	/// 			<term><see cref="EncryptionKey"/></term>
+	/// 			<description>"SEK"</description>
+	/// 		</item>
+	/// 	</list>
+	/// </remarks>
+	/// 
+	/// <exception cref="ArgumentNullException">
+	/// 	Thrown if "SSK" or "SEK" are not defined on the "JWT" section
+	/// 	of <paramref name="config"/>
+	/// </exception>
+	[SetsRequiredMembers]
+	public TokenBuilderParams(IConfiguration config, Func<string, byte[]> converter)
+	{
+		var sect = config.GetSection("JWT");
+		SigningKey = new SymmetricSecurityKey(
+			converter(sect["SSK"] ?? throw new ArgumentNullException($"{nameof(config)}[\"JWT__SSK\"]"))
+		);
+		EncryptionKey = new SymmetricSecurityKey(
+			converter(sect["SEK"] ?? throw new ArgumentNullException($"{nameof(config)}[\"JWT__SEK\"]"))
+		);
+	}
+
 	/// <summary>
 	/// 	Key for signing tokens.
 	/// </summary>
